2558511105c2058c00db28bff747ed5b5f3b387f
Rodrigo Medina <rodrigo.medina.neri@gmail.com>
2020-08-27 11:31:47 -0500

    Added new stuff

--- dot/hammerspoon/init.lua
+++ dot/hammerspoon/init.lua
@@ -1,5 +1,11 @@
 hyper = {"cmd","alt"}
 
+hs.loadSpoon("SpoonInstall")
+spoon.SpoonInstall.use_syncinstall = true
+Install=spoon.SpoonInstall
+
+Install:andUse("UnsplashRandom")
+
 -- Keybindings for launching apps
 appKeys = {
   c = "Google Chrome",
@@ -13,6 +19,7 @@ appKeys = {
   t = "iTerm",
   x = "Firefox Developer Edition",
 }
+
 -- Bind appKeys
 for key, app in pairs(appKeys)
 do
@@ -20,3 +27,15 @@ do
                    hs.application.launchOrFocus(app)
   end)
 end
+
+-- FadeLogo is last to ensure that it confirms hammerspoon has fully loaded successfully
+Install:andUse("FadeLogo",
+               {
+                 config = {
+                   fade_in_time = 0,
+                   run_time = 0,
+                   fade_out_time = 0.4
+                 },
+                 start = true
+               }
+)

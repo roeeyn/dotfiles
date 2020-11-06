518eaab468120a058055babcf440d8c6f97bf773
Rodrigo Medina <rodrigo.medina.neri@gmail.com>
2020-11-05 22:13:41 -0600

    Added more stuff

--- dot/hammerspoon/init.lua
+++ dot/hammerspoon/init.lua
@@ -1,43 +1,78 @@
-hyper = {"cmd","alt"}
+hyper = {"cmd", "alt"}
 
 hs.loadSpoon("SpoonInstall")
 spoon.SpoonInstall.use_syncinstall = true
-Install=spoon.SpoonInstall
+Install = spoon.SpoonInstall
 
 Install:andUse("UnsplashRandom")
 
 -- Keybindings for launching apps
 appKeys = {
-  c = "Google Chrome",
-  d = "Finder",
-  e = "Emacs",
-  f = "Franz",
-  k = "Calendar",
-  p = "Postman",
-  s = "Spotify",
-  t = "iTerm",
-  v = "Visual Studio Code",
-  x = "Firefox Developer Edition",
+    c = "Google Chrome",
+    d = "Finder",
+    e = "Emacs",
+    f = "Franz",
+    k = "Calendar",
+    p = "Postman",
+    s = "Spotify",
+    t = "iTerm",
+    v = "Visual Studio Code",
+    x = "Firefox Developer Edition"
 }
 
-hs.hotkey.bind({"cmd", "alt", "shift"}, "V", function() hs.eventtap.keyStrokes(hs.pasteboard.getContents()) end)
+Install:andUse(
+    "Caffeine",
+    {
+        hotkeys = {
+            toggle = {hyper, "/"}
+        },
+        start = true
+    }
+)
+
+hs.hotkey.bind(
+    {"cmd", "alt", "shift"},
+    "V",
+    function()
+        hs.eventtap.keyStrokes(hs.pasteboard.getContents())
+    end
+)
+
+hs.hotkey.bind(
+    hyper,
+    "b",
+    function()
+        hs.notify.new(
+            {
+                title = "Battery",
+                autoWithdraw = false,
+                informativeText = "Charged: " ..
+                    hs.battery.percentage() .. "%\nRemaining Time: " .. hs.battery.timeRemaining() .. " min"
+            }
+        ):send()
+    end
+)
 
 -- Bind appKeys
-for key, app in pairs(appKeys)
-do
-  hs.hotkey.bind(hyper, key, function()
-                   hs.application.launchOrFocus(app)
-  end)
+for key, app in pairs(appKeys) do
+    hs.hotkey.bind(
+        hyper,
+        key,
+        function()
+            hs.application.launchOrFocus(app)
+        end
+    )
 end
 
 -- FadeLogo is last to ensure that it confirms hammerspoon has fully loaded successfully
-Install:andUse("FadeLogo",
-               {
-                 config = {
-                   fade_in_time = 0,
-                   run_time = 0,
-                   fade_out_time = 0.4
-                 },
-                 start = true
-               }
+Install:andUse(
+    "FadeLogo",
+    {
+        config = {
+            fade_in_time = 0,
+            run_time = 0,
+            fade_out_time = 0.4
+        },
+        start = true
+    }
 )

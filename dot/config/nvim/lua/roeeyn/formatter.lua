374de9b180823ef84925b668720e33a9d2144117
Rodrigo Medina <rodrigom@lyft.com>
2021-05-27 19:18:02 -0500

    Synced with lyft branch

--- dot/config/nvim/lua/roeeyn/formatter.lua
+++ dot/config/nvim/lua/roeeyn/formatter.lua
@@ -1,6 +1,15 @@
 require'formatter'.setup{
    logging = true,
    filetype = {
+      json = {
+         function()
+            return {
+               exe = "prettier",
+               args = {"--stdin-filepath ", vim.api.nvim_buf_get_name(0),""},
+               stdin = true
+            }
+         end
+      },
       typescript = {
          function()
             return {

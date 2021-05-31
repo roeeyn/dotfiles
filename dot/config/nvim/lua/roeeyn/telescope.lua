e0bb3f588ce9d6b4629f99ad03e222446ec24c70
Rodrigo Medina <rodrigom@lyft.com>
2021-05-31 15:15:54 -0500

    Formatted lua files

--- dot/config/nvim/lua/roeeyn/telescope.lua
+++ dot/config/nvim/lua/roeeyn/telescope.lua
@@ -1,13 +1,9 @@
 require("telescope").setup {
-  defaults = {
-    mappings = {
-      n = {
-        ["<C-q>"] = require("telescope.actions").smart_send_to_qflist
-      },
-      i = {
-        ["<C-q>"] = require("telescope.actions").smart_send_to_qflist
-      }
+    defaults = {
+        mappings = {
+            n = {["<C-q>"] = require("telescope.actions").smart_send_to_qflist},
+            i = {["<C-q>"] = require("telescope.actions").smart_send_to_qflist}
+        }
     }
-  }
 }
 

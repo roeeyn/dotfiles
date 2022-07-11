f52ce9b487f355ec9f6acae517624e0e6b9a24ce
Rodrigo Medina <rodrigom@lyft.com>
2022-07-11 07:56:45 -0500

    Adding new mappings for comment and reconfig ufo

--- dot/.config/nvim/lua/roeeyn/plugins/nvim_ufo.lua
+++ dot/.config/nvim/lua/roeeyn/plugins/nvim_ufo.lua
@@ -1,13 +1,11 @@
 -- Nvim UFO (Folding)
--- tell the sever the capability of foldingRange
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.foldingRange = {
-    dynamicRegistration = false,
-    lineFoldingOnly = true
-}
 
 vim.wo.foldcolumn = '1'
 vim.wo.foldlevel = 99 -- feel free to decrease the value
 vim.wo.foldenable = false
 
-require('ufo').setup()
+require('ufo').setup({
+    provider_selector = function(bufnr, filetype)
+        return {'treesitter', 'indent'}
+    end
+})

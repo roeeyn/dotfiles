48bd86c5c3ba2531dd910d7bfcd4deced05de233
Rodrigo Medina <rodrigom@lyft.com>
2023-02-07 21:28:58 -0600

    Adding latest lyft changes

--- dot/.hammerspoon/init.lua
+++ dot/.hammerspoon/init.lua
@@ -9,15 +9,9 @@ Install:andUse 'UnsplashZ'
 -- Keybindings for launching apps
 local appKeys = {
   a = 'Slack',
-  c = 'Visual Studio Code',
-  d = 'DataGrip',
-  l = 'Telegram',
-  m = 'Spark',
   p = 'Postman',
-  s = 'Spotify',
+  r = 'Arc',
   t = 'Alacritty',
-  v = 'Vivaldi',
-  w = 'WhatsApp',
   x = 'Firefox Developer Edition',
 }
 

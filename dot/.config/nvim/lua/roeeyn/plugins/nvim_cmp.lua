dfc89251b135fbd6c2b4aa4eea7ac4852f127247
Rodrigo Medina <rodrigom@lyft.com>
2023-03-30 19:40:35 -0600

    Fixed nvim cmp

--- dot/.config/nvim/lua/roeeyn/plugins/nvim_cmp.lua
+++ dot/.config/nvim/lua/roeeyn/plugins/nvim_cmp.lua
@@ -1,5 +1,6 @@
 -- Setup nvim-cmp.
 local cmp = require 'cmp'
+local lspkind = require 'lspkind'
 
 if cmp == nil then
   return
@@ -12,23 +13,18 @@ cmp.setup {
     end,
   },
   formatting = {
-    format = function(entry, vim_item)
-      -- fancy icons and a name of kind
-      vim_item.kind = require('lspkind').presets.default[vim_item.kind] .. ' ' .. vim_item.kind
+    format = lspkind.cmp_format {
+      mode = 'symbol', -- show only symbol annotations
+      maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+      ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
 
-      -- set a name for each source
-      vim_item.menu = ({
-        buffer = '[Buffer]',
-        nvim_lsp = '[LSP]',
-        nvim_lua = '[Lua]',
-        cmp_tabnine = '[TabNine]',
-        path = '[Path]',
-        spell = '[Spell]',
-        calc = '[Calc]',
-        emoji = '[Emoji]',
-      })[entry.source.name]
-      return vim_item
-    end,
+      -- The function below will be called before any actual modifications from lspkind
+      -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+      before = function(entry, vim_item)
+        -- ...
+        return vim_item
+      end,
+    },
   },
   window = {
     -- completion = cmp.config.window.bordered(),
@@ -88,10 +84,10 @@ cmp.setup.filetype('gitcommit', {
 })
 
 -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline('/', {
+cmp.setup.cmdline({ '/', '?' }, {
   mapping = cmp.mapping.preset.cmdline(),
   sources = {
-    { name = 'buffer', keyword_length = 3 },
+    { name = 'buffer' },
   },
 })
 
@@ -101,6 +97,6 @@ cmp.setup.cmdline(':', {
   sources = cmp.config.sources({
     { name = 'path' },
   }, {
-    { name = 'cmdline', keyword_length = 3 },
+    { name = 'cmdline' },
   }),
 })

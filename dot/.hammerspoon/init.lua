ef5fd48373aaf43702340b68a45d9c04c38cfcf7
Rodrigo Medina <rodrigo.medina.neri@gmail.com>
2023-07-03 21:39:37 -0600

    feat: add random background spoon config in hammerspoon init lua

--- dot/.hammerspoon/init.lua
+++ dot/.hammerspoon/init.lua
@@ -4,42 +4,51 @@ hs.loadSpoon 'SpoonInstall'
 spoon.SpoonInstall.use_syncinstall = true
 Install = spoon.SpoonInstall
 
-Install:andUse 'UnsplashZ'
+Install:andUse('RandomBackground', {
+    loglevel = 'debug',
+    config = {
+        unsplash_api_key = os.getenv 'UNSPLASH_API_KEY',
+    },
+    start = true,
+})
 
 -- Keybindings for launching apps
 local appKeys = {
-  a = 'Slack',
-  o = 'Obsidian',
-  p = 'Postman',
-  r = 'Arc',
-  t = 'Alacritty',
-  x = 'Firefox Developer Edition',
+    a = 'Slack',
+    o = 'Obsidian',
+    p = 'Postman',
+    r = 'Arc',
+    t = 'Alacritty',
+    x = 'Firefox Developer Edition',
 }
 
 Install:andUse('Caffeine', {
-  hotkeys = {
-    toggle = { hyper, '/' },
-  },
-  start = true,
+    hotkeys = {
+        toggle = { hyper, '/' },
+    },
+    start = true,
 })
 
+Caffeine = spoon.Caffeine
+Caffeine:setState(true)
+
 hs.hotkey.bind({ 'cmd', 'shift' }, 'V', function()
-  hs.eventtap.keyStrokes(hs.pasteboard.getContents())
+    hs.eventtap.keyStrokes(hs.pasteboard.getContents())
 end)
 
 -- Bind appKeys
 for key, app in pairs(appKeys) do
-  hs.hotkey.bind(hyper, key, function()
-    hs.application.launchOrFocus(app)
-  end)
+    hs.hotkey.bind(hyper, key, function()
+        hs.application.launchOrFocus(app)
+    end)
 end
 
 -- FadeLogo is last to ensure that it confirms hammerspoon has fully loaded successfully
 Install:andUse('FadeLogo', {
-  config = {
-    fade_in_time = 0,
-    run_time = 0,
-    fade_out_time = 0.4,
-  },
-  start = true,
+    config = {
+        fade_in_time = 0,
+        run_time = 0,
+        fade_out_time = 0.4,
+    },
+    start = true,
 })

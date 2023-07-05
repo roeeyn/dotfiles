8c225284ad1d207786efb6ad4a5511fd40c087b4
Rodrigo Medina <r.medina@kolors.com.mx>
2023-07-05 11:53:17 -0600

    feat: replace unsplash api usage

--- dot/.hammerspoon/init.lua
+++ dot/.hammerspoon/init.lua
@@ -4,10 +4,27 @@ hs.loadSpoon 'SpoonInstall'
 spoon.SpoonInstall.use_syncinstall = true
 Install = spoon.SpoonInstall
 
+local function read_api_key(file_path)
+    local file = io.open(file_path, 'r') -- r read mode
+    if not file then
+        return nil
+    end
+
+    local content = file:read '*a' -- *a or *all reads the whole file
+    file:close()
+
+    return content:gsub('%s+$', '') -- remove trailing spaces
+end
+
+-- TODO: set a better place for this
+local api_key = read_api_key './Spoons/RandomBackground.spoon/.unsplash_api_key'
+
+print('Unsplash API Key: ' .. api_key)
+
 Install:andUse('RandomBackground', {
     loglevel = 'debug',
     config = {
-        unsplash_api_key = os.getenv 'UNSPLASH_API_KEY',
+        unsplash_api_key = api_key,
     },
     start = true,
 })

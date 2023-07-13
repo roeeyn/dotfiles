1672d03ee13586b5169cd2096fde6061351a9320
Rodrigo Medina <rodrigo.medina.neri@gmail.com>
2023-07-12 19:50:10 -0600

    feat: add random background spoon

--- dot/.hammerspoon/init.lua
+++ dot/.hammerspoon/init.lua
@@ -4,10 +4,24 @@ hs.loadSpoon 'SpoonInstall'
 spoon.SpoonInstall.use_syncinstall = true
 Install = spoon.SpoonInstall
 
+local function read_api_key(file_path)
+    local file = io.open(file_path, 'r') -- r read mode
+    if not file then
+        return nil
+    end
+
+    local content = file:read '*a' -- *a or *all reads the whole file
+    file:close()
+
+    return content:gsub('%s+$', '') -- remove trailing spaces
+end
+
+local api_key = read_api_key(os.getenv 'HOME' .. '/.unsplash_api_key')
+
 Install:andUse('RandomBackground', {
     loglevel = 'debug',
     config = {
-        unsplash_api_key = os.getenv 'UNSPLASH_API_KEY',
+        unsplash_api_key = api_key,
     },
     start = true,
 })

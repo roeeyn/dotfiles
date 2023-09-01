820a9451464b15a67b7f3a31281b2cee548cd82d
Rodrigo Medina <r.medina@kolors.com.mx>
2023-09-01 11:55:52 -0600

    refactor(hammerspoon): remove unused spoons in favor of raycast

--- dot/.hammerspoon/init.lua
+++ dot/.hammerspoon/init.lua
@@ -1,5 +1,3 @@
-local hyper = { 'cmd', 'alt' }
-
 hs.loadSpoon 'SpoonInstall'
 spoon.SpoonInstall.use_syncinstall = true
 Install = spoon.SpoonInstall
@@ -26,26 +24,6 @@ Install:andUse('RandomBackground', {
     start = true,
 })
 
--- Keybindings for launching apps
-local appKeys = {
-    a = 'Slack',
-    o = 'Obsidian',
-    p = 'Postman',
-    r = 'Arc',
-    t = 'Alacritty',
-    x = 'Firefox Developer Edition',
-}
-
-Install:andUse('Caffeine', {
-    hotkeys = {
-        toggle = { hyper, '/' },
-    },
-    start = true,
-})
-
-Caffeine = spoon.Caffeine
-Caffeine:setState(true)
-
 hs.hotkey.bind({ 'cmd', 'shift' }, 'V', function()
     hs.eventtap.keyStrokes(hs.pasteboard.getContents())
 end)
@@ -56,13 +34,6 @@ Install:andUse('FocusHighlight', {
     },
     start = true,
 })
-
--- Bind appKeys
-for key, app in pairs(appKeys) do
-    hs.hotkey.bind(hyper, key, function()
-        hs.application.launchOrFocus(app)
-    end)
-end
 
 -- FadeLogo is last to ensure that it confirms hammerspoon has fully loaded successfully
 Install:andUse('FadeLogo', {

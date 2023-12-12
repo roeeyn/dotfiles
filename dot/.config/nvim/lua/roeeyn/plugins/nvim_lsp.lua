23bee60952104b10909247cd652b86af46f2a248
Rodrigo Medina <r.medina@kolors.com.mx>
2023-12-12 12:50:05 -0600

    feat: add graphql to lsp

--- dot/.config/nvim/lua/roeeyn/plugins/nvim_lsp.lua
+++ dot/.config/nvim/lua/roeeyn/plugins/nvim_lsp.lua
@@ -1,55 +1,56 @@
 -- LSP Configuration
 --  This function gets run when an LSP connects to a particular buffer.
 local on_attach = function(_, bufnr)
-  -- TODO: TJ told me to do this and I should do it because he is Telescopic
-  -- "Big Tech" "Cash Money" Johnson
-  -- Create a command `:Format` local to the LSP buffer
-  vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-    vim.lsp.buf.format()
-  end, { desc = 'Format current buffer with LSP' })
+    -- TODO: TJ told me to do this and I should do it because he is Telescopic
+    -- "Big Tech" "Cash Money" Johnson
+    -- Create a command `:Format` local to the LSP buffer
+    vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+        vim.lsp.buf.format()
+    end, { desc = 'Format current buffer with LSP' })
 end
 
 local folding_capabilities = vim.lsp.protocol.make_client_capabilities()
 folding_capabilities.textDocument.foldingRange = {
-  dynamicRegistration = false,
-  lineFoldingOnly = true,
+    dynamicRegistration = false,
+    lineFoldingOnly = true,
 }
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities(folding_capabilities)
 
 local servers = {
-  ansiblels = {},
-  dockerls = {},
-  bashls = {},
-  gopls = {},
-  emmet_ls = {},
-  tsserver = {},
-  pyright = {
-    python = {
-      venv_path = '~/.pyenv/versions',
+    ansiblels = {},
+    dockerls = {},
+    bashls = {},
+    gopls = {},
+    graphql = {},
+    emmet_ls = {},
+    tsserver = {},
+    pyright = {
+        python = {
+            venv_path = '~/.pyenv/versions',
+        },
     },
-  },
-  rust_analyzer = {},
-  html = {},
-  svelte = {},
-  jsonls = {},
-  lua_ls = {
-    Lua = {
-      diagnostics = {
-        globals = { 'hs', 'spoon', 'vim' },
-      },
-      workspace = { checkThirdParty = false },
-      telemetry = { enable = false },
+    rust_analyzer = {},
+    html = {},
+    svelte = {},
+    jsonls = {},
+    lua_ls = {
+        Lua = {
+            diagnostics = {
+                globals = { 'hs', 'spoon', 'vim' },
+            },
+            workspace = { checkThirdParty = false },
+            telemetry = { enable = false },
+        },
     },
-  },
 }
 
 -- Setup Formatter and Linter
 local null_ls = require 'null-ls'
 null_ls.setup {
-  sources = {
-    null_ls.builtins.formatting.stylua,
-  },
+    sources = {
+        null_ls.builtins.formatting.stylua,
+    },
 }
 
 -- Setup mason so it can manage external tooling
@@ -59,15 +60,15 @@ require('mason').setup()
 local mason_lspconfig = require 'mason-lspconfig'
 
 mason_lspconfig.setup {
-  ensure_installed = vim.tbl_keys(servers),
+    ensure_installed = vim.tbl_keys(servers),
 }
 
 mason_lspconfig.setup_handlers {
-  function(server_name)
-    require('lspconfig')[server_name].setup {
-      capabilities = capabilities,
-      on_attach = on_attach,
-      settings = servers[server_name],
-    }
-  end,
+    function(server_name)
+        require('lspconfig')[server_name].setup {
+            capabilities = capabilities,
+            on_attach = on_attach,
+            settings = servers[server_name],
+        }
+    end,
 }

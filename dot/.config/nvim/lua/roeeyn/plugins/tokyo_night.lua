660702377dcea9ae99f41210e6ba1ebfaa7794d6
Rodrigo Medina <r.medina@kolors.com.mx>
2023-12-21 14:04:53 -0600

    feat: switched to day theme

--- dot/.config/nvim/lua/roeeyn/plugins/tokyo_night.lua
+++ dot/.config/nvim/lua/roeeyn/plugins/tokyo_night.lua
@@ -1,25 +1,25 @@
 -- Theme configuration
 require('tokyonight').setup {
-  style = 'night',
-  styles = {
-    functions = {
-      italic = true,
+    style = 'day',
+    styles = {
+        functions = {
+            italic = true,
+        },
     },
-  },
-  on_colors = function(colors)
-    colors.fg_gutter = '#555f8b'
-  end,
-  on_highlights = function(hl, c)
-    hl.CursorLineNr = {
-      fg = c.cyan,
-    }
-    hl.Visual = {
-      bg = '#2f334d',
-    }
-    hl.ColorColumn = {
-      bg = '#222436',
-    }
-  end,
+    on_colors = function(colors)
+        colors.fg_gutter = '#555f8b'
+    end,
+    on_highlights = function(hl, c)
+        hl.CursorLineNr = {
+            fg = c.cyan,
+        }
+        hl.Visual = {
+            bg = '#2f334d',
+        }
+        hl.ColorColumn = {
+            bg = '#222436',
+        }
+    end,
 }
 
 vim.cmd [[colorscheme tokyonight]]

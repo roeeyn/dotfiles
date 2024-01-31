139aedc7d59d3a5948c1d13ee5ec7bcf9ec8553b
Rodrigo Medina <rodrigo.medina.neri@gmail.com>
2024-01-31 15:30:05 -0600

    feat: install tokyo theme

---
+++ dot/.config/nvim/lua/config.lua
@@ -0,0 +1,197 @@
+-- Nvim Tree
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- Needed settings for font issues (Telescope)
+vim.opt.encoding = 'utf-8'
+
+-- Highlight search
+vim.opt.hlsearch = true
+
+-- Column limit 120
+vim.opt.colorcolumn = '120'
+
+-- Cursor line
+vim.opt.cursorline = true
+
+-- Move to underscore words
+vim.opt.iskeyword = vim.opt.iskeyword - '_'
+
+-- Reads changes from external events
+vim.opt.autoread = true
+
+-- Needed for cool colors
+vim.opt.termguicolors = true
+
+-- Is how many columns of whitespace a tab keypress or a backspace keypress is worth
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+
+-- How many columns of whitespace a level of identation is worth
+vim.opt.shiftwidth = 4
+
+-- Use spaces instead of \t
+vim.opt.expandtab = true
+
+-- The cursor is moved onto the window with 'scrolloff' screen lines around it
+vim.opt.scrolloff = 8
+
+-- Set line numbers
+vim.opt.number = true
+
+-- Set relative line numbers
+vim.opt.relativenumber = true
+
+-- Ignore folders in searche
+vim.opt.wildignore = '*/node_modules/*,*/target/*,*/tmp/*,*/venv/*'
+
+-- Show quotes in JSON
+vim.opt.conceallevel = 0
+
+-- Gitgutter to catch the changes faster
+vim.opt.updatetime = 100
+vim.g.gitgutter_map_keys = 0
+
+-- Timeout for whichkey to trigger
+vim.opt.timeoutlen = 600
+
+-- DoGe Global parameters
+vim.g.doge_enable_mappings = 0
+vim.g.doge_doc_standard_python = 'google'
+vim.g.doge_comment_jump_modes = { 'n', 's' }
+
+-- Winbar Config (Top right with modifier)
+vim.opt.winbar = '%=%m %f'
+
+-- Split vertically to the right
+vim.opt.splitright = true
+
+-- Snippets
+vim.g.snippets = 'luasnip'
+vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
+
+-- Case insensitive searching UNLESS /C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- AUTOCOMMANDS
+
+vim.api.nvim_create_autocmd('FileType', {
+    -- To add more files, transform the string to table {}
+    pattern = 'python',
+    callback = function()
+        vim.schedule(function()
+            vim.bo.tabstop = 4
+            vim.bo.shiftwidth = 4
+            vim.bo.softtabstop = 4
+        end)
+    end,
+})
+
+-- Clearing red for empty spaces in scratch
+local empty_spaces_ag = vim.api.nvim_create_augroup('highlight-trailing-spaces', { clear = true })
+
+vim.cmd 'highlight ExtraWhitespace ctermbg=red guibg=red'
+
+vim.api.nvim_create_autocmd('FileType', {
+    group = empty_spaces_ag,
+    pattern = '*',
+    command = [[match ExtraWhitespace /\s\+$/]],
+})
+
+local alpha_filetype = 'dashboard'
+
+vim.api.nvim_create_autocmd('FileType', {
+    group = empty_spaces_ag,
+    pattern = { alpha_filetype, 'help', 'mason', 'fugitive', 'lspinfo', 'TelescopePrompt', 'TelescopeResults' },
+    command = 'match none',
+})
+
+vim.api.nvim_create_autocmd('TermOpen', {
+    group = empty_spaces_ag,
+    pattern = '*',
+    command = 'match none',
+})
+
+vim.api.nvim_create_autocmd('User', {
+    group = empty_spaces_ag,
+    pattern = 'AlphaReady',
+    command = 'set filetype=' .. alpha_filetype,
+})
+
+-- Adding comment string for elixir patterns
+local set_comment_string_ag = vim.api.nvim_create_augroup('set-commentstring-ag', { clear = true })
+
+-- When you enter a new buffer
+vim.api.nvim_create_autocmd('BufEnter', {
+    group = set_comment_string_ag,
+    pattern = 'elixir',
+    callback = function()
+        vim.api.nvim_buf_set_option(0, 'commentstring', '# %s')
+    end,
+})
+
+-- When you've changed the name of a file opened in a buffer and the file type may have changed
+vim.api.nvim_create_autocmd('BufFilePost', {
+    group = set_comment_string_ag,
+    pattern = 'elixir',
+    callback = function()
+        vim.api.nvim_buf_set_option(0, 'commentstring', '# %s')
+    end,
+})
+
+-- Setting comment string for prisma
+vim.api.nvim_create_autocmd('FileType', {
+    group = set_comment_string_ag,
+    pattern = 'prisma',
+    callback = function()
+        vim.api.nvim_buf_set_option(0, 'commentstring', '// %s')
+    end,
+})
+
+-- Highlight Yanked text
+local yank_ag = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+    group = yank_ag,
+    callback = function()
+        vim.highlight.on_yank { timeout = 250, on_visual = true }
+    end,
+})
+
+----------------------------------------------------------------------
+--                List of all the keymaps registered                --
+----------------------------------------------------------------------
+
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+local function map(mode, shortcut, command)
+    vim.api.nvim_set_keymap(mode, shortcut, command, {
+        noremap = true,
+        -- TODO: Verify if this is ok
+        -- silent = true,
+    })
+end
+
+local function vmap(shortcut, command)
+    map('v', shortcut, command)
+end
+
+local function xmap(shortcut, command)
+    map('x', shortcut, command)
+end
+
+local function tmap(shortcut, command)
+    map('t', shortcut, command)
+end
+
+-- Terminal
+tmap('<esc>', '<C-\\><C-n>')
+
+-- Moving lines up and down in visual mode
+vmap('J', ":m '>+1<CR>gv=gv")
+vmap('K', ":m '<-2<CR>gv=gv")
+
+-- Yanking in visual mode to clipboard
+xmap('<leader>y', '"+y<CR>')
---
+++ dot/.config/nvim/lua/plugins.lua
@@ -0,0 +1,121 @@
+-- Plugin Manager bootstrapping
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system {
+        'git',
+        'clone',
+        '--filter=blob:none',
+        'https://github.com/folke/lazy.nvim.git',
+        '--branch=stable', -- latest stable release
+        lazypath,
+    }
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+-- Plugins Config
+require('lazy').setup {
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.5',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            'telescope-fzf-native.nvim',
+        },
+        opts = function(plugin, opts)
+            local actions = require 'telescope.actions'
+
+            return {
+                pickers = {
+                    colorscheme = {
+                        enable_preview = true,
+                    },
+                    find_files = {
+                        hidden = true,
+                    },
+                    live_grep = {
+                        ---@diagnostic disable-next-line: unused-local
+                        additional_args = function(opts)
+                            return {
+                                '--hidden',
+                            }
+                        end,
+                    },
+                },
+                extensions = {
+                    media_files = {
+                        find_cmd = 'rg', -- find command (defaults to `fd`)
+                    },
+                },
+                defaults = {
+                    dynamic_preview_title = true,
+                    mappings = {
+                        n = {
+                            ['<C-q>'] = actions.smart_send_to_qflist,
+                            ['<C-x>'] = actions.delete_buffer,
+                        },
+                        i = {
+                            ['<C-q>'] = actions.smart_send_to_qflist,
+                            ['<C-x>'] = actions.delete_buffer,
+                        },
+                    },
+                },
+            }
+        end,
+        config = function(plugin, opts)
+            require('telescope').setup(opts)
+            require('telescope').load_extension 'fzf'
+        end,
+        cmd = 'Telescope',
+    },
+    {
+        'folke/tokyonight.nvim',
+        lazy = false, -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000, -- make sure to load this before all the other start plugins
+        opts = function()
+            local function get_theme_based_on_time()
+                local hour = tonumber(os.date '%H')
+
+                local day_theme = 'day'
+                local night_theme = 'moon'
+
+                if hour >= 8 and hour <= 18 then
+                    return day_theme
+                else
+                    return night_theme
+                end
+            end
+
+            local theme = get_theme_based_on_time()
+            return {
+                style = theme,
+                styles = {
+                    functions = {
+                        italic = true,
+                    },
+                },
+                on_colors = function(colors)
+                    colors.fg_gutter = '#555f8b'
+                end,
+                on_highlights = function(hl, c)
+                    hl.CursorLineNr = {
+                        fg = c.cyan,
+                    }
+                    -- hl.Visual = {
+                    --     bg = '#2f334d',
+                    -- }
+                    -- hl.ColorColumn = {
+                    --     bg = '#222436',
+                    -- }
+                end,
+            }
+        end,
+        config = function(plugin, opts)
+            -- load the colorscheme here
+            require('tokyonight').setup(opts)
+
+            vim.cmd [[colorscheme tokyonight]]
+        end,
+    },
+}

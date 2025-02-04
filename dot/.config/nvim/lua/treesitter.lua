9d7fbcc2513ea6dbc53c18d25883d0cbb8338ac4
Rodrigo Medina <rodrigo.medina.neri@gmail.com>
2025-02-04 13:03:00 -0600

    chore: update deps

--- dot/.config/nvim/lua/treesitter.lua
+++ dot/.config/nvim/lua/treesitter.lua
@@ -1,6 +1,6 @@
 require('treesitter-context').setup {
     enable = true,
-    max_lines = 0, -- no max lines if <= 0
+    max_lines = 10, -- no max lines if <= 0
     zindex = 50,
     patterns = {
         default = {

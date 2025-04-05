b22fc9b652d676e56e2615a2e4bc65ab19cb0b6d
Rodrigo Medina <rodrigo.medina.neri@gmail.com>
2025-04-04 22:10:12 -0600

    refactor: remove amethyst and aerospace

--- dot/.config/sketchybar/items/calendar.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sketchybar --add item calendar right \
-            --set calendar icon=󰃰 \
-                    update_freq=30 \
-                    script="$PLUGIN_DIR/calendar.sh"

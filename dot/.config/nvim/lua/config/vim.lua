033b820750663679d4dc8d8cf6caa22bfe78df15
Rodrigo Medina <rodrigo.medina.neri@gmail.com>
2025-09-11 22:28:37 -0600

    add more vim configs

--- dot/.config/nvim/lua/config/vim.lua
+++ dot/.config/nvim/lua/config/vim.lua
@@ -4,6 +4,9 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+-- Needed settings for font issues (Telescope)
+vim.opt.encoding = 'utf-8'
+
 -- Cursor line
 vim.opt.cursorline = true
 
@@ -12,6 +15,16 @@ vim.opt.iskeyword = vim.opt.iskeyword - '_'
 
 -- Reads changes from external events
 vim.opt.autoread = true
+
+-- Is how many columns of whitespace a tab keypress or a backspace keypress is worth
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+
+-- How many columns of whitespace a level of identation is worth
+vim.opt.shiftwidth = 4
+
+-- Use spaces instead of \t
+vim.opt.expandtab = true
 
 -- The cursor is moved onto the window with 'scrolloff' screen lines around it
 vim.opt.scrolloff = 8
@@ -28,6 +41,10 @@ vim.opt.winbar = '%=%m %f'
 -- Split vertically to the right
 vim.opt.splitright = true
 
+-- Case insensitive searching UNLESS /C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
 -- Clearing red for empty spaces in scratch
 local empty_spaces_ag = vim.api.nvim_create_augroup('highlight-trailing-spaces', { clear = true })
 
@@ -37,6 +54,12 @@ vim.api.nvim_create_autocmd('FileType', {
     group = empty_spaces_ag,
     pattern = '*',
     command = [[match ExtraWhitespace /\s\+$/]],
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+    group = empty_spaces_ag,
+    pattern = { 'help', 'mason', 'fugitive', 'lspinfo', 'TelescopePrompt', 'TelescopeResults' },
+    command = 'match none',
 })
 
 -- Highlight Yanked text

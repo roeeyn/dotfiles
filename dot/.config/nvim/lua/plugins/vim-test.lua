3688f1d5a1bb90bd191a5d6116001ea9fa60614f
Rodrigo Medina <rodrigo.medina.neri@gmail.com>
2025-10-13 22:41:19 -0600

    update vim test config

--- dot/.config/nvim/lua/plugins/vim-test.lua
+++ dot/.config/nvim/lua/plugins/vim-test.lua
@@ -4,5 +4,11 @@ return {
         vim.g['test#strategy'] = 'neovim'
         vim.g['test#neovim#term_position'] = 'vert botright'
         vim.g['test#neovim#start_normal'] = 1 -- Start in normal mode so we can scroll
+
+        -- Check if we're in the messaging_stats_service project
+        local cwd = vim.fn.getcwd()
+        if string.match(cwd, 'messaging_stats_service') then
+            vim.g['test#elixir#exunit#executable'] = 'dockc run --rm -e MIX_ENV=test mss mix test'
+        end
     end,
 }

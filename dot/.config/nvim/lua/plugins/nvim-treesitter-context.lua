3de8cdfec9d56b7728af38796e5043c7c69e3e2f
Rodrigo Medina <rodrigo.medina.neri@gmail.com>
2026-02-10 20:46:13 -0600

    feat: enabling treesitter context

---
+++ dot/.config/nvim/lua/plugins/nvim-treesitter-context.lua
@@ -0,0 +1,9 @@
+return {
+    'nvim-treesitter/nvim-treesitter-context',
+    config = function()
+        require('treesitter-context').setup {
+            enable = true,
+            max_lines = 3,
+        }
+    end,
+}

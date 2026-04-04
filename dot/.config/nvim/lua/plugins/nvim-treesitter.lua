8d926402ad0d288cdf0385f0539db0aa6bc3a645
Rodrigo Medina <rodrigo.medina.neri@gmail.com>
2026-04-03 22:45:19 -0600

    remove commented config

--- dot/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ dot/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -2,12 +2,4 @@ return {
     'nvim-treesitter/nvim-treesitter',
     lazy = false,
     build = ':TSUpdate',
-    -- config = function()
-    --     require('nvim-treesitter.configs').setup {
-    --         highlight = {
-    --             enable = true,
-    --             additional_vim_regex_highlighting = false,
-    --         },
-    --     }
-    -- end,
 }

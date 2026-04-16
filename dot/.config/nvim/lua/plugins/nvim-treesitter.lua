c0b38d1f6ab8477982679d394e5b06bc698f8306
Rodrigo Medina <rodrigo.medina.neri@gmail.com>
2026-04-15 18:24:13 -0600

    fix: TS now working

--- dot/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ dot/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,5 +1,34 @@
+local filetypes = {
+    'bash',
+    'css',
+    'dockerfile',
+    'elixir',
+    'heex',
+    'html',
+    'javascript',
+    'json',
+    'lua',
+    'markdown',
+    'python',
+    'typescript',
+    'vim',
+    'vimdoc',
+    'yaml',
+}
+
 return {
     'nvim-treesitter/nvim-treesitter',
     lazy = false,
     build = ':TSUpdate',
+    config = function()
+        require('nvim-treesitter').install(filetypes)
+
+        vim.api.nvim_create_autocmd('FileType', {
+            pattern = filetypes,
+            callback = function()
+                vim.treesitter.start()
+                vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+            end,
+        })
+    end,
 }
